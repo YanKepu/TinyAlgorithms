b1d73d1a3955031985a4ae80e15360ae11a69778
YanKepu <yan_kepu@163.com>
2025-01-19 22:52:12 +0800

    2025-1-19: The division of modules

---
+++ leetcode101/Part1 Basic data structures/Data_Structure.c
@@ -0,0 +1,19 @@
+/* 
+    11 妙用数据结构
+*/
+#include <stdio.h>
+#include <stdlib.h>
+/* 数组 */
+/*
+    448. Find All Numbers Disappeared in an Array
+    题目描述：给定一个长度为n的数组，其中包含范围为1 到n的整数，有些整数重复了多次，有些整数
+没有出现，求1 到n中没有出现过的整数。
+    Input： [4,3,2,7,8,2,3,1]
+    Output: [5,6]
+*/
+
+int findDisapperedNumbers(int* nums, int* ans)
+{
+    
+    return ans;
+}
---
+++ leetcode101/Part1 Basic data structures/readme.md
@@ -0,0 +1,4 @@
+Part1 Basic data structures
+    本部分主要包括数据结构、链表定义等，初版会根据LeetCode101的章节进行分类。包括11、13、14、15、16章节。
+
+
